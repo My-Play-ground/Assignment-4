22678e1fbe39e0066d0f2e5f9ef0fca88d66008d
Janaka Daskon <janakadaskon@gmail.com>
2023-08-12 23:39:01 +0530

    finished Fibonacci using do-while loop

--- Question1.java
+++ Question1.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class Question1{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int n = scan.nextInt();
         int x1 = 0;
         int x2 = 1;
+        int i = 1;
         
         do{
-            System.out.print("Enter a positive number: ");
-            int i = scan.nextInt();
+            System.out.println(x1 + " ");
             
             int x3 = x1 + x2;
             x1 = x2;
             x2 = x3;
-        }
+            
+            i++;
+
+        }while(i<=n);
+        
     }
 }
