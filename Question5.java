78462e7cf58fbba3569de48fcd6dbeb6f0157658
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 12:20:43 +0530

    q5

---
+++ Question5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Question5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter ASCII only text: ");
+
+        if (!input.isEmpty()){
+            String revText = reverseString(input);
+
+        }
+    }
+}
