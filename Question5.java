a46b6b4a2019b947ea22cc846a254ac32748ff1b
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 19:11:08 +0530

    Finished

--- Question5.java
+++ Question5.java
@@ -20,5 +20,15 @@ public class Question5 {
     private static String reverseString(String input) {
         char[] charArray = str.toCharArray();
         int left = 0;
+        int right = charArray.length - 1;
+
+        while(left<right){
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right]= temp;
+            left++;
+            right++;
+        }
+        return new String(charArray);
     }
 }
