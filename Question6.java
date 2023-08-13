d5d442a530f843fc4392239f7c0dfdb0ef51a0e6
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 20:00:20 +0530

    if condition

--- Question6.java
+++ Question6.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Question6{
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine();
+        
         
     }
 }
