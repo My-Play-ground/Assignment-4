5f39f0ea93aa0fc868035e3cc41c5a1b5bb8c026
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 20:50:10 +0530

    q8

---
+++ Question8.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Question8 {
+    public static void main(String[] args) {
+        int[] numA = {5, 7, -2, 3, 4, 6, 7};
+        int[] numB = {7, 8, -8, 2, 1, -9, 6};
+
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+
+}
