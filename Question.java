22318a8e2d0374cf04e3065488e46c670969582f
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 11:34:35 +0530

    Initiate the problem

---
+++ Question.java
@@ -0,0 +1,10 @@
+public class Question {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+        
+        int[] revArray = new int[nums.length];
+        for (int i = ;i < nums.length; i++);
+
+
+    } 
+}
