57d5ea39a472d25c16ee2407645f68effdf46f69
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 20:22:28 +0530

    q7 finished

--- Question7.java
+++ Question7.java
@@ -9,6 +9,25 @@ public class Question7 {
 
         if (sentence.isEmpty()) {
             System.out.println("Input sentence is empty.");
+        } else {
+            String[] words = sentence.split("\\s+");
+            String shortestWord = words[0];
+            String longestWord = words[0];
+
+            for (String word : words) {
+                if (word.length() < shortestWord.length()) {
+                    shortestWord = word;
+                }
+                if (word.length() > longestWord.length()) {
+                    longestWord = word;
+                }
+            }
+
+            System.out.println("Shortest word: " + shortestWord + " (Length: " + shortestWord.length() + ")");
+            System.out.println("Longest word: " + longestWord + " (Length: " + longestWord.length() + ")");
+        }
+
+        scanner.close();
 
     }
 }
