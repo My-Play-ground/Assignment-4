957aec6bbfea1617ae555a2f76c5899f8172a9bc
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 19:04:46 +0530

    Defined reverseString class

--- Question5.java
+++ Question5.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 public class Question5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter ASCII only text: ");
+        System.out.print("Enter (ASCII only) text: ");
+
+        String input= scan.nextLine();
 
         if (!input.isEmpty()){
-            String revText = reverseString(input);
+            String reversedText = reverseString(input);
+            System.out.println("Reversed Text: " + reversedText);
 
         }
+    }
+
+    private static String reverseString(String input) {
+        return null;
     }
 }
