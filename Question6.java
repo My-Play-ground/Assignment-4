b5f8b0e62217f08907b7a0725b864192369cd68e
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 20:17:22 +0530

    q6 finished

--- Question6.java
+++ Question6.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
-public class Question6{
+public class Question6 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a sentence: ");
         String input = scanner.nextLine();
 
-        if(input.trim().isEmpty()){
-            System.out.println("Input is empty,Try again: ");
-
-        }else{
+        if (input.trim().isEmpty()) {
+            System.out.println("Input is empty. Please enter a valid sentence.");
+        } else {
             String[] words = input.split("\\s+");
             StringBuilder reversedSentence = new StringBuilder();
-            for(int i = -words.length - 1;i>= 0 ;i--){
-                
+            for (int i = words.length - 1; i >= 0; i--) {
+                reversedSentence.append(words[i]);
+                if (i > 0) {
+                    reversedSentence.append(" ");
+                }
             }
 
+            System.out.println("Reversed sentence: " + reversedSentence);
         }
-        
+
+        scanner.close();
     }
 }
