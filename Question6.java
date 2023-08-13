933fdd97c803ae7a9f245422f197ea487d9b562a
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 20:04:18 +0530

    for loop

--- Question6.java
+++ Question6.java
@@ -6,7 +6,18 @@ public class Question6{
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
         String input = scanner.nextLine();
-        
+
+        if(input.trim().isEmpty()){
+            System.out.println("Input is empty,Try again: ");
+
+        }else{
+            String[] words = input.split("\\s+");
+            StringBuilder reversedSentence = new StringBuilder();
+            for(int i = -words.length - 1;i>= 0 ;i--){
+                
+            }
+
+        }
         
     }
 }
