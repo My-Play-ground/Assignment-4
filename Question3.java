e4004e1266dfd826d30241a57b91b7194659841c
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 12:00:46 +0530

    Q4 started

--- Question3.java
+++ Question3.java
@@ -1,4 +1,4 @@
-public class Question {
+public class Question3 {
     public static void main(String[] args) {
         int[] nums = {10, 20, 30, 40, 50};
         
---
+++ Question4.java
@@ -0,0 +1,11 @@
+public class Question4 {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        
+        
+    }
+}
