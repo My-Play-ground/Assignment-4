1d68e236c5611ab969659669b53349f1657cf6d6
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 11:46:08 +0530

    solved

--- Question.java
+++ Question.java
@@ -3,7 +3,14 @@ public class Question {
         int[] nums = {10, 20, 30, 40, 50};
         
         int[] revArray = new int[nums.length];
-        for (int i = 0;i < nums.length; i++);
+        for (int i = 0;i < nums.length; i++){
+            revArray[i] = nums[nums.length -1 -i];
+
+        }
+        nums = revArray;
+        for (int num : nums){
+            System.out.println(num + " ");
+        }
 
 
     } 
