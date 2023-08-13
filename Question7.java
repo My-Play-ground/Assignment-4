a3a549c8330456e97ccacad932a64aaf00141c59
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 20:20:55 +0530

    if started

--- Question7.java
+++ Question7.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Question7 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine().trim();
+
+        if (sentence.isEmpty()) {
+            System.out.println("Input sentence is empty.");
+
     }
 }
