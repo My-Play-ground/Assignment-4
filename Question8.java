7208c408940f3c632ec3074f9f6519e71acbca81
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 20:51:46 +0530

    q8 finished

--- Question8.java
+++ Question8.java
@@ -8,5 +8,32 @@ public class Question8 {
 
         Set<Integer> setA = new HashSet<>();
         Set<Integer> setB = new HashSet<>();
+            for (int num : numA) {
+                setA.add(num);
+            }
 
+            for (int num : numB) {
+                setB.add(num);
+            }
+
+        Set<Integer> intersection = new HashSet<>(setA);
+        intersection.retainAll(setB);
+
+        Set<Integer> union = new HashSet<>(setA);
+        union.addAll(setB);
+
+        Set<Integer> difference = new HashSet<>(setA);
+        difference.removeAll(setB);
+
+        Set<Integer> symmetricDifference = new HashSet<>(union);
+        symmetricDifference.removeAll(intersection);
+
+        System.out.println("1. numA: " + setA);
+        System.out.println("2. numB: " + setB);
+        System.out.println("3. Intersection: " + intersection);
+        System.out.println("∪ Union: " + union);
+        System.out.println("/ Difference: " + difference);
+        System.out.println("14. Symmetric Difference: " + symmetricDifference);
+        System.out.println("5. numA / numA △ numB: " + setA);
+    }
 }
