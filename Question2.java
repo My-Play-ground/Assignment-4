490e79632c80ab3b856fa9e68a4f20caeb4ff4e5
Janaka Daskon <janakadaskon@gmail.com>
2023-08-13 10:10:32 +0530

    initiate the array

--- Question2.java
+++ Question2.java
@@ -1,5 +1,8 @@
 public class Question2 {
     public static void main(String[] args) {
+        int[] numbers = {-5, 2, 7, 10, 58, -7, 8, 23};
+        int largest = numbers[0];
+        int smallest = numbers[0];
         
     }
 }
